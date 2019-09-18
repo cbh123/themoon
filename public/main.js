617508afa7248ab2369365b2459d4a378e66307a
Charlie Holtz <charlieholtz@gmail.com>
2019-09-17 21:46:26 -0400

    packages / visit

--- public/main.js
+++ public/main.js
@@ -11,3 +11,16 @@ $("#touchdown").click(function() {
     window.location = "home.html";
   });
 });
+
+// db.collection("users")
+//   .add({
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+//   })
+//   .then(function(docRef) {
+//     console.log("Document written with ID: ", docRef.id);
+//   })
+//   .catch(function(error) {
+//     console.error("Error adding document: ", error);
+//   });
